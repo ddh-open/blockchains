f0dcaa213a338716ae73dcdd1c8f6f3bfc429138
Daohu Dou（一见消） <daohu.dou@basebit.ai>
2023-04-12 11:49:01 +0800

    01

--- blk/block.go
+++ blk/block.go
@@ -1,5 +1,32 @@
 package blk
 
-// Block 区块链结构
+import (
+	"time"
+)
+
 type Block struct {
+	//1. 区块高度
+	Height int64
+	//2. 上一个区块HASH
+	PrevBlockHash []byte
+	//3. 交易数据
+	Data []byte
+	//4. 时间戳
+	Timestamp int64
+	//5. Hash
+	Hash []byte
+	// 6. Nonce
+	Nonce int64
+}
+
+// NewBlock 创建新的区块
+func NewBlock(data string, height int64, prevBlockHash []byte) *Block {
+	//创建区块
+	block := &Block{height, prevBlockHash, []byte(data), time.Now().Unix(), nil, 0}
+	return block
+}
+
+// CreateGenesisBlock 生成创世区块
+func CreateGenesisBlock(data string) *Block {
+	return NewBlock(data, 1, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
 }
