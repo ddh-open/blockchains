2afc19f38945ce6539d3227756aa901a4a879384
Daohu Dou（一见消） <daohu.dou@basebit.ai>
2023-04-13 16:13:57 +0800

    增加 区块持久化

--- blk/block_chain.go
+++ blk/block_chain.go
@@ -1,19 +1,74 @@
 package blk
 
+import (
+	"github.com/boltdb/bolt"
+	"log"
+)
+
+const dbName = "blockchain.db"
+
+const blockTableName = "block"
+
 type BlockChain struct {
-	Blocks []*Block
+	Tips []byte
+	DB   *bolt.DB
 }
 
 // AddBlockToBlockChain 添加区块
-func (bc *BlockChain) AddBlockToBlockChain(data string, height int64, prevBlockHash []byte) {
-	block := NewBlock(data, height, prevBlockHash)
-	bc.Blocks = append(bc.Blocks, block)
+func (bc *BlockChain) AddBlockToBlockChain(data string) {
+	err := bc.DB.Update(func(tx *bolt.Tx) error {
+		// 获取表
+		b := tx.Bucket([]byte(blockTableName))
+		if b != nil {
+			blockBytes := b.Get(bc.Tips)
+			block := Deserialize(blockBytes)
+
+			newBlock := NewBlock(data, block.Height+1, block.Hash)
+			err := b.Put(newBlock.Hash, newBlock.Serialize())
+			if err != nil {
+				log.Panic(err)
+			}
+			bc.Tips = newBlock.Hash
+		}
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
 }
 
 // CreateBlockChainWithGenesisBlock 创世区块
 func CreateBlockChainWithGenesisBlock() *BlockChain {
-	genesisBlock := CreateGenesisBlock("Genesis Block......")
-	return &BlockChain{
-		Blocks: append([]*Block{}, genesisBlock),
+	db, err := bolt.Open(dbName, 0600, nil)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	var blockHash []byte
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		// 获取表
+		b := tx.Bucket([]byte(blockTableName))
+
+		if b == nil {
+			b, err = tx.CreateBucket([]byte(blockTableName))
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		if b != nil {
+			genesisBlock := CreateGenesisBlock("Genesis Block......")
+			err = b.Put(genesisBlock.Hash, genesisBlock.Serialize())
+
+			if err != nil {
+				log.Panic(err)
+			}
+			blockHash = genesisBlock.Hash
+		}
+
+		return nil
+	})
+
+	return &BlockChain{blockHash, db}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module blockchains
 
 go 1.19
+
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+)
